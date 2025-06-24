53cf4523e5838f2896690be0b2054bcc0c3513d3
HauHuynh2k4 <ngochau1310@gmail.com>
2025-06-24 21:53:34 +0700

    add toxic waterfall and auto gate

---
+++ Assets/Scripts/Map5/AutoGate.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+
+public class AutoGate : MonoBehaviour
+{
+    public Animator doorAnimator;  // Gắn Animator của cửa vào đây trong Inspector
+
+    private int playerCount = 0;   // Số lượng phần collider của Player trong vùng cửa
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            playerCount++;
+            if (playerCount == 1)
+            {
+                // Mở cửa nếu Player vừa mới bước vào
+                doorAnimator.ResetTrigger("CloseTrigger");
+                doorAnimator.SetTrigger("OpenTrigger");
+            }
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            playerCount--;
+            if (playerCount <= 0)
+            {
+                // Đóng cửa khi Player đã đi hết qua vùng trigger
+                doorAnimator.ResetTrigger("OpenTrigger");
+                doorAnimator.SetTrigger("CloseTrigger");
+            }
+        }
+    }
+}
