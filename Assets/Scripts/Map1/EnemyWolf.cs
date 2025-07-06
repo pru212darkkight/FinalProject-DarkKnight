314d7c3f0110a60d598f67aaf522eacdfb8d13ad
Truc Phuong <maitrucphuong0111@gmail.com>
2025-07-06 12:30:27 +0700

    enemy ani

--- Assets/Scripts/Map1/EnemyWolf.cs
+++ Assets/Scripts/Map1/EnemyWolf.cs
@@ -53,7 +53,7 @@ public class Enemy : MonoBehaviour
         {
             isFacingRight = player.position.x > transform.position.x;
             Vector3 scale = transform.localScale;
-            scale.x = Mathf.Abs(scale.x) * (isFacingRight ? -1 : 1);
+            scale.x = Mathf.Abs(scale.x) * (isFacingRight ? 1 : -1);
             transform.localScale = scale;
         }
     }
