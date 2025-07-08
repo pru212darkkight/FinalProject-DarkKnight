a7bf05c505c3878dc28203c871bc486d99b3902d
Truc Phuong <maitrucphuong0111@gmail.com>
2025-07-08 16:03:40 +0700

    pack

---
+++ Assets/Scripts/Player/PlayerInteraction.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+
+public class PlayerInteraction : MonoBehaviour
+{
+    public GameObject interactionUI; // UI hiện chữ "Nhấn E để nói chuyện"
+    private bool isNearNPC = false;
+    private NPCDialogue currentNPC;
+
+    void Update()
+    {
+        if (isNearNPC && Input.GetKeyDown(KeyCode.E))
+        {
+            currentNPC.StartDialogue();
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("NPC"))
+        {
+            isNearNPC = true;
+            currentNPC = other.GetComponent<NPCDialogue>();
+            interactionUI.SetActive(true);
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.CompareTag("NPC"))
+        {
+            isNearNPC = false;
+            currentNPC = null;
+            interactionUI.SetActive(false);
+        }
+    }
+}
