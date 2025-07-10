4be7bf558d6febcb93c79fbd0bb07030ba78a6ce
ToTuyen04 <tuyentmse183888@fpt.edu.vn>
2025-07-10 23:57:09 +0700

    add animation

--- Assets/Scripts/Map3/BossDamage.cs
+++ Assets/Scripts/Map3/BossDamage.cs
@@ -2,15 +2,64 @@ using UnityEngine;
 
 public class BossDamage : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    [Header("Damage Settings")]
+    public float damage = 10f;
+    public bool isMagicDamage = false;
+
+    [Header("Auto Find Player")]
+    public bool autoFindPlayer = true;
+    public PlayerController1 playerController;
+
     void Start()
     {
-        
+        // Auto find player if not assigned
+        if (autoFindPlayer && playerController == null)
+        {
+            GameObject playerObj = GameObject.FindGameObjectWithTag("Player");
+            if (playerObj != null)
+            {
+                playerController = playerObj.GetComponent<PlayerController1>();
+                Debug.Log("BossDamage: Player found and assigned!");
+            }
+            else
+            {
+                Debug.LogWarning("BossDamage: Player not found!");
+            }
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnCollisionEnter2D(Collision2D collision)
     {
-        
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            PlayerController1 player = collision.gameObject.GetComponent<PlayerController1>();
+            if (player != null)
+            {
+                player.TakeDamage(damage, isMagicDamage);
+                Debug.Log($"Player hit by boss, damage taken: {damage}");
+            }
+            else
+            {
+                Debug.LogWarning("BossDamage: Player doesn't have PlayerController1 component!");
+            }
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            PlayerController1 player = other.GetComponent<PlayerController1>();
+            if (player != null)
+            {
+                player.TakeDamage(damage, isMagicDamage);
+                Debug.Log($"Player hit by boss (trigger), damage taken: {damage}");
+            }
+            else
+            {
+                Debug.LogWarning("BossDamage: Player doesn't have PlayerController1 component!");
+            }
+        }
     }
 }
+
